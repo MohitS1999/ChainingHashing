c66c57a989dc6f4bea033d89de70e776c460d1d5
Mohit singh <singhmohit9718@gmail.com>
2020-05-12 00:31:48 +0530

    Hashing

---
+++ ChainingHashing.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	public Node(int data){
+		this.data=data;
+		this.next=null;
+	}
+}
+public class ChainingHashing{
+	public static void main(String[] args){
+		Node a[]=new Node[10];
+		for(int i=0;i<10;i++)
+			a[i]=null;
+		
+		Insert(a,12);
+		Insert(a,12);
+		Insert(a,12);
+		for(int i=0;i<10;i++)
+			System.out.println(a[i]);
+	}
+	static void Insert(Node a[],int key){
+		int index=hash(key);
+		LinkedList l=new LinkedList();
+		a[index]=l;
+		l.insert(a[index],key);
+	}
+	static int hash(int key){
+		return key%10;
+	}
+}
+class LinkedList{
+	public void insert(Node head,int key){
+		Node n=head;
+		Node node=new Node(key);
+		Node q=null;
+		while((n!=null) && (n.data<key)){
+			q=n;
+			n=n.next;
+		}
+		if(n==head){
+			node.next=head;
+			head=node;	
+		}else{
+			node.next=q.next;
+			q.next=node;
+		}
+		Node root
+	}
+}
